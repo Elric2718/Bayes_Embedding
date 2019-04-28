2ae429d664e5928a9285aa30bb250e120ff63dc6
Elric2718 <yeyt@berkeley.edu>
2019-04-28 10:20:29 -0700

    comment the codes

--- end2end/graphsage/code/bem_e2e.py
+++ end2end/graphsage/code/bem_e2e.py
@@ -17,8 +17,10 @@ h_func = lambda x: tf.tanh(x)
 h_func_prime = lambda x: 1 - tf.tanh(x) ** 2
 eps = 1e-7
 
-# planar flow
 def planar_flow(z0, z_dim, length=8, reuse=False):
+    """
+    planar flow module
+    """
     z_prev = z0
     for k in range(length):
         with tf.variable_scope('planar_flow_layer_%d' % k, reuse=reuse):
@@ -30,8 +32,11 @@ def planar_flow(z0, z_dim, length=8, reuse=False):
     zK = z_prev
     return zK
 
-# normalization flow to model multi-modal distributions
+
 def SLDJ(z0, z_dim, length=8, reuse=False):
+    """
+    normalization flow to model multi-modal distributions
+    """
     z_prev = z0
     sum_log_det_jacob = 0.
     for k in range(length):
@@ -47,6 +52,7 @@ def SLDJ(z0, z_dim, length=8, reuse=False):
 
 class BayesNetwork(object):
     """
+    pipeline concatenate the end2end BEM with the BG method.
     """
 
     def __init__(self,
@@ -62,12 +68,8 @@ class BayesNetwork(object):
                  learning_rate = 0.001,\
                  lambda1 = 1,\
                  lambda2 = 1,\
-                 #n_epoch = 100,\
-                 nf_K = 8,\
-                 pair_or_single = "pair"):#,\
-                 #seed = 0,\
-                 #checkpoint_path = 'checkpoints/model.ckpt',\
-                 #log_file = None):
+                 nf_K = 0,\
+                 pair_or_single = "pair"):
         """
         Initialize the object.
         
@@ -76,10 +78,15 @@ class BayesNetwork(object):
             n_w: demensionality of the variable of prior info.
             n_hidden: number of hidden neurons.
             n_z: demensionality of the observed variable.
+            w1_inputs: w1 embeddings
+            w2_inputs: w2 embeddings (might include negative samples)
+            z1_inputs: z1 embeddings
+            z2_inputs: z2 embeddings (might include negative samples)
+            mapping_index: indexes that generate the batches
             learning_rate: the initial learning rate of the optimization algorithm.
-            n_epoch: number of epochs to train the autoencoder.
-            seed: a random seed to create reproducible results.
-            checkpoint_path: path to the optimized model parameters.
+            lambda1, lambda2: two parameters to balance the kg side and the bg side.
+            nf_K: the number of flows. Default as 0
+            pair_or_single: BEM type. 'pair' or 'single' 
         """
         self.epsilon = 1e-8
         self.n_batch = n_batch
@@ -103,49 +110,6 @@ class BayesNetwork(object):
         
         #self.learning_curve = {'train': [], 'val': []}                 
 
-    # def _create_graph(self):
-    #     """
-    #     Return the computational graph.
-    #     """
-
-    #     with tf.Graph().as_default() as graph:
-    #         tf.set_random_seed(self.seed)
-    #         self.loss, self.regu_term1, self.regu_term2, self.regu_term3, self.regu_term4 = self._create_model()
-    #         self.optimizer = self._create_optimizer(self.loss, self.learning_rate)
-    #         self.initializer = tf.global_variables_initializer()
-    #         self.saver = tf.train.Saver()
-    #         graph.finalize()
-    #     return graph
-
-    # def _restore_model(self, session):
-    #     """
-    #     Restores the model from the model's checkpoint path.
-        
-    #     Args:
-    #         session: current session which should hold the graph to which the model parameters are assigned.
-    #     """
-
-    #     self.saver.restore(session, self.checkpoint_path)
-        
-    # def _create_encoder(self, input_dat):
-    #     """
-    #     not used
-    #     """
-        
-    #     h = layers.Dense(self.n_hidden, activation='relu')(input_dat)
-    #     mu_delta1 = layers.Dense(self.n_w)(h)
-    #     log_sigma_squared_delta1 = layers.Dense(self.n_w)(h)
-        
-    #     mu_delta2 = layers.Dense(self.n_w)(h)
-    #     log_sigma_squared_delta2 = layers.Dense(self.n_w)(h)
-        
-    #     mu_s1 = layers.Dense(self.n_z)(h)
-    #     log_sigma_squared_s1 = layers.Dense(self.n_z)(h)
-        
-    #     mu_s2 = layers.Dense(self.n_z)(h)        
-    #     log_sigma_squared_s2 = layers.Dense(self.n_z)(h)
-
-    #     return mu_delta1, log_sigma_squared_delta1, mu_delta2, log_sigma_squared_delta2, mu_s1, log_sigma_squared_s1, mu_s2, log_sigma_squared_s2
 
     def _create_encoder_v2(self, input_dat):
         """
@@ -222,23 +186,11 @@ class BayesNetwork(object):
         (z_2, h_2).
         """
 
-        # self.z1 = tf.placeholder(tf.float32, [None, self.n_z])
-        # self.z2 = tf.placeholder(tf.float32, [None, self.n_z])
-        # self.w1 = tf.placeholder(tf.float32, [None, self.n_w])
-        # self.w2 = tf.placeholder(tf.float32, [None, self.n_w])
-
         self.w1 = tf.gather(self.w1_inputs, tf.transpose(self.mapping_index)[0])
         self.w2 = tf.gather(self.w2_inputs, tf.transpose(self.mapping_index)[1])
         self.z1 = tf.gather(self.z1_inputs, tf.transpose(self.mapping_index)[0])
         self.z2 = tf.gather(self.z2_inputs, tf.transpose(self.mapping_index)[1])
 
-        # _create_encoder
-        #self.mu_delta1, self.log_sigma_squared_delta1, self.mu_delta2, self.log_sigma_squared_delta2, self.mu_s1, self.log_sigma_squared_s1, self.mu_s2, self.log_sigma_squared_s2 = self._create_encoder(tf.concat([self.z1, self.z2], axis = 1))
-        # _create_encoder_v2
-        # conditional on only z
-        # self.mu_delta1, self.log_sigma_squared_delta1, self.mu_s1, self.log_sigma_squared_s1 = self._create_encoder_v2(self.z1)
-        # self.mu_delta2, self.log_sigma_squared_delta2, self.mu_s2, self.log_sigma_squared_s2 = self._create_encoder_v2(self.z2)
-        # conditional on both z and h
 
         self.mu_delta1, self.log_sigma_squared_delta1, self.mu_s1, self.log_sigma_squared_s1 = self._create_encoder_v2(tf.concat([self.z1, self.w1], axis = 1))
         if self.pair_or_single == "pair":
@@ -271,7 +223,6 @@ class BayesNetwork(object):
         else:
             self.generated_z1, self.new_w1, self.f1 = self._create_decoder(self.w1 + self.delta1_K, None, self.sigma_squared_s1, None)
 
-        #print(self.log_sigma_squared_s1, self.log_sigma_squared_s1_prior, self.mu_s1, self.mu_s1_prior)
         # regularization term
         if self.nf_K > 0:
             regular_term1 = tfd.MultivariateNormalDiag(loc = self.mu_delta1_prior, scale_diag = tf.exp(self.log_sigma_squared_delta1_prior/2.)).log_prob(self.delta1_K) - tfd.MultivariateNormalDiag(loc = self.mu_delta1, scale_diag = tf.exp(self.log_sigma_squared_delta1/2.)).log_prob(self.delta1) + SLDJ(self.delta1, z_dim = self.n_w, length = self.nf_K, reuse = True)
@@ -315,143 +266,10 @@ class BayesNetwork(object):
         
     def _compute_loss(self, session, batch, optimize = True):
         """
+        output the loss that can be combined with the loss of the bg side (implemented) or the kg side (not implemented yet).
         """
 
         ops = [self.loss, self.regu_term1, self.regu_term2, self.regu_term3, self.regu_term4, self.optimizer] if optimize else [self.loss, self.regu_term1, self.regu_term2, self.regu_term3, self.regu_term4]
         loss, regu_term1, regu_term2, regu_term3, regu_term4 = session.run(ops, {self.w1: batch["w1"], self.w2: batch["w2"], self.z1: batch["z1"], self.z2: batch["z2"]})[0:5]
 
-        return loss, regu_term1, regu_term2, regu_term3, regu_term4
-        
-    # def fit(self, train, validation):
-    #     """
-    #     """
-
-        
-    #     self.graph = self._create_graph() # Create the computational graph
-    #     config = tf.ConfigProto()
-    #     config.gpu_options.allow_growth = True
-    #     self.sess = tf.Session(graph=self.graph, config = config)
-    #     #coord = tf.train.Coordinator() # Let tensorflow handle multiple threads.
-    #     #threads = tf.train.start_queue_runners(self.sess, coord) 
-    #     self.sess.run(self.initializer) # Initialize the network.
-    #     num_steps_in_epoch = train.num_examples // self.n_batch * train.Decare_rounds
-    #     n_step = num_steps_in_epoch * self.n_epoch 
-
-    #     start = time.time()
-    #     np.random.seed(self.seed) # Fix the seed used for shuffling the input data
-
-    #     try:
-    #         self.learning_curve['train'].clear()
-    #         self.learning_curve['val'].clear()
-    #         loss_train = 0.
-    #         loss_train_regu1 = 0.
-    #         loss_train_regu2 = 0.
-    #         loss_train_regu3 = 0.
-    #         loss_train_regu4 = 0.
-    #         loss_val = 0.
-    #         loss_val_regu1 = 0.
-    #         loss_val_regu2 = 0.
-    #         loss_val_regu3 = 0.
-    #         loss_val_regu4 = 0.
-
-
-    #         for step in range(n_step):
-    #             loc_loss_train, loc_loss_train_regu1, loc_loss_train_regu2, loc_loss_train_regu3, loc_loss_train_regu4 = self._compute_loss(self.sess, batch = train.next_batch(self.n_batch), optimize=True)
-    #             loc_loss_val, loc_loss_val_regu1, loc_loss_val_regu2, loc_loss_val_regu3, loc_loss_val_regu4 = self._compute_loss(self.sess, batch = validation.next_batch(int(self.n_batch)), optimize=False)
-
-    #             loss_train += loc_loss_train
-    #             loss_train_regu1 += loc_loss_train_regu1
-    #             loss_train_regu2 += loc_loss_train_regu2
-    #             loss_train_regu3 += loc_loss_train_regu3
-    #             loss_train_regu4 += loc_loss_train_regu4
-    #             loss_val += loc_loss_val
-    #             loss_val_regu1 += loc_loss_val_regu1
-    #             loss_val_regu2 += loc_loss_val_regu2
-    #             loss_val_regu3 += loc_loss_val_regu3
-    #             loss_val_regu4 += loc_loss_val_regu4
-                
-
-    #             if (step + 1) % num_steps_in_epoch == 0:
-    #                 train_error = self.n_batch / train.num_examples * loss_train
-    #                 val_error = self.n_batch / validation.num_examples * loss_val
-    #                 train_error_regu1 = self.n_batch / train.num_examples * loss_train_regu1
-    #                 train_error_regu2 = self.n_batch / train.num_examples * loss_train_regu2
-    #                 train_error_regu3 = self.n_batch / train.num_examples * loss_train_regu3
-    #                 train_error_regu4 = self.n_batch / train.num_examples * loss_train_regu4
-    #                 val_error_regu1 = self.n_batch / validation.num_examples * loss_val_regu1
-    #                 val_error_regu2 = self.n_batch / validation.num_examples * loss_val_regu2
-    #                 val_error_regu3 = self.n_batch / validation.num_examples * loss_val_regu3
-    #                 val_error_regu4 = self.n_batch / validation.num_examples * loss_val_regu4                                                                                
-    #                 # Return the negative error to allow monitoring for the ELBO.
-    #                 self.learning_curve['train'] += [train_error]
-    #                 self.learning_curve['val'] += [val_error]
-    #                 loss_train = 0.
-    #                 loss_train_regu1 = 0.
-    #                 loss_train_regu2 = 0.
-    #                 loss_train_regu3 = 0.
-    #                 loss_train_regu4 = 0.
-    #                 loss_val = 0.
-    #                 loss_val_regu1 = 0.
-    #                 loss_val_regu2 = 0.
-    #                 loss_val_regu3 = 0.
-    #                 loss_val_regu4 = 0.
-
-    #                 store_msg = '---------------------------------\n---------------------------------\n '\
-    #                   'epoch: {epoch_id}\n step: {steps} \n training error: {train_error}\n validation error: {val_error}\n '\
-    #                   'train error (regu1): {train_error_regu1}\n train error (regu2): {train_error_regu2}\n '\
-    #                   'train error (regu3): {train_error_regu3}\n train error (regu4): {train_error_regu4}\n '\
-    #                   'val error (regu1): {val_error_regu1}\n val error (regu2): {val_error_regu2}\n '\
-    #                   'val error (regu3): {val_error_regu3}\n val error (regu4): {val_error_regu4}\n '\
-    #                   'time elapsed: {time_elapsed} s\n'.format(epoch_id = train.epochs_completed,\
-    #                                                             steps = step,\
-    #                                                             train_error = train_error,\
-    #                                                             val_error =  val_error,\
-    #                                                             train_error_regu1 = train_error_regu1,\
-    #                                                             train_error_regu2 = train_error_regu2,\
-    #                                                             train_error_regu3 = train_error_regu3,\
-    #                                                             train_error_regu4 = train_error_regu4,\
-    #                                                             val_error_regu1 = val_error_regu1,\
-    #                                                             val_error_regu2 = val_error_regu2,\
-    #                                                             val_error_regu3 = val_error_regu3,\
-    #                                                             val_error_regu4 = val_error_regu4,\
-    #                                                             time_elapsed = time.time() - start)
-    #                 print(store_msg)
-    #                 if ((step + 1)/num_steps_in_epoch <= 5\
-    #                    or (step + 1)/num_steps_in_epoch >= self.n_epoch - 5)\
-    #                    and os.path.isfile(self.log_file):
-    #                     write_file=open(self.log_file, "a+")
-    #                     write_file.write(store_msg)
-    #                     write_file.close()
-                    
-    #     except KeyboardInterrupt:
-    #         print('ending training')
-    #     finally:
-    #         # If interrupted or stopped, store the progress of the model.
-    #         self.saver.save(self.sess, self.checkpoint_path)
-    #         self.sess.close()
-    #         #coord.request_stop()
-    #         #coord.join(threads)
-    #         print('finished training')
-    #     return self                  
-
-    # def encode(self, z, h):
-    #     """
-    #     """
-    #     config = tf.ConfigProto()
-    #     config.gpu_options.allow_growth = True
-    #     with tf.Session(graph=self.graph, config = config) as session:
-    #         self._restore_model(session)
-    #         mu_delta1 = session.run([self.mu_delta1], {self.z1: z, self.w1: h})
-    #     return np.array(mu_delta1)
-
-    # def decode(self, mu, h):
-    #     """
-    #     """
-    #     config = tf.ConfigProto()
-    #     config.gpu_options.allow_growth = True
-    #     with tf.Session(graph=self.graph, config = config) as session:
-    #         self._restore_model(session)
-    #         f1, new_w1 = session.run([self.f1, self.new_w1], {self.w1: h, self.delta1: mu})
-            
-    #     return np.array(f1), np.array(new_w1)
-   
+        return loss, regu_term1, regu_term2, regu_term3, regu_term4          
